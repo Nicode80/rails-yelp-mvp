c10020973ddd35e7d390fbe3205888fd2677b2e3
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-11 15:27:10 +0100

    E-bouffe works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "create 5 fake resto"
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORIES.sample
+    )
+  puts "Created #{restaurant.name}"
+end
+puts "done"
